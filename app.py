9b126291c4762a8340ede0b6dbd84092b28583f8
Jiri Hubacek <jirihubacek@Aryas-MacBook-Pro.local>
2020-08-03 22:35:09 +1000

    WIP

--- app.py
+++ app.py
@@ -3,8 +3,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def home():
     return "<h1>Hello World!</h1>"
+
+@app.route("/<guid>")
+def download(guid):
+    return f"<h1>Get file: {guid}</h1>"
 
 if __name__ == "__main__":
     app.run()
