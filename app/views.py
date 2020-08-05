3d3d1b4463399cb7040eb0970ab92bd00ab97cd0
Jiri Hubacek <hubacekjirka@users.noreply.github.com>
2020-08-05 11:58:28 +1000

    Organise project, add basic templates (#1)
    Organize project
    Add basic templates
    Add navigation

---
+++ app/views.py
@@ -0,0 +1,18 @@
+from app import app
+from flask import render_template
+
+@app.route('/', methods=["GET", "POST"])
+def home():
+    return render_template('index.html')
+
+@app.route('/upload', methods=["GET", "POST"])
+def upload():
+    return render_template('upload.html')
+
+@app.route("/about")
+def about():
+    return render_template('about.html')
+
+@app.route("/<guid>")
+def download(guid):
+    return f"<h1>Get file: {guid}</h1>"
