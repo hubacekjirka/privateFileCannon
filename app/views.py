c746f6101b50883712332a8ed1d69cbd1398a299
Jiri Hubacek <jirihubacek@Aryas-MacBook-Pro.local>
2020-08-10 00:45:28 +1000

    WIP

--- app/views.py
+++ app/views.py
@@ -1,13 +1,22 @@
 from app import app
-from flask import render_template
+from flask import render_template, request, redirect
+import os
 
-@app.route('/', methods=["GET", "POST"])
+#TBD: put into config,py
+app.config["fileStoragePath"] = ""
+
+@app.route("/", methods=["GET", "POST"])
 def home():
-    return render_template('upload.html')
+    if request.method == "POST":
+        if request.files:
+            file = request.files["file"]
+            file.save(app.config["fileStoragePath"], file.filename)
+            return redirect(request.url)
+    return render_template("upload.html")
 
 @app.route("/about")
 def about():
-    return render_template('about.html')
+    return render_template("about.html")
 
 @app.route("/<guid>")
 def download(guid):
