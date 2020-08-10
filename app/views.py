e5cfc7ba39c7521b9480de0b8bc44ddccf92c6b8
Jiri Hubacek <jirihubacek@Aryas-MacBook-Pro.local>
2020-08-11 00:33:01 +1000

    done

--- app/views.py
+++ app/views.py
@@ -1,16 +1,16 @@
 from app import app
-from flask import render_template, request, redirect
+from flask import render_template, request, redirect, send_from_directory, abort
 from werkzeug.utils import secure_filename
 import os
 
 #TBD: put into config,py
 
 currentDir = os.path.dirname(os.path.realpath(__file__))
-app.config["fileStoragePath"] = os.path.join(currentDir,"fileStorage")
-app.config["maxFileSize"] = 100 * 1024 * 1024
+app.config["file_storage_path"] = os.path.join(currentDir,"fileStorage")
+app.config["max_file_size"] = 100 * 1024 * 1024
 
-def allowedImageFilesize(fileSize):
-    if int(fileSize) <= app.config["maxFileSize"]:
+def allowed_image_filesize(file_size):
+    if int(file_size) <= app.config["max_file_size"]:
         return True
 
     return False
@@ -20,9 +20,9 @@ def home():
     if request.method == "POST":
         print(request.cookies)
         if request.files:
-            if not allowedImageFilesize(request.cookies.get("fileSize")):
+            if not allowed_image_filesize(request.cookies.get("fileSize")):
                 #TODO: do flash message
-                print(f"File exceeded maximum size of {app.config['maxFileSize'] / 1024 / 1024}MB")
+                print(f"File exceeded maximum size of {app.config['max_file_size'] / 1024 / 1024}MB")
                 return redirect(request.url)
 
             file = request.files["file"]
@@ -32,9 +32,9 @@ def home():
                 print("Image must have a filename")
                 return redirect(request.url)
 
-            fileName = secure_filename(file.filename)
+            file_name = secure_filename(file.filename)
 
-            file.save(os.path.join(app.config["fileStoragePath"], fileName))
+            file.save(os.path.join(app.config["file_storage_path"], file_name))
             return redirect(request.url)
     return render_template("upload.html")
 
@@ -42,6 +42,26 @@ def home():
 def about():
     return render_template("about.html")
 
-@app.route("/<guid>")
-def download(guid):
-    return f"<h1>Get file: {guid}</h1>"
+#TODO:
+#@app.route("/<uuid:guid>")
+#def download(guid):
+#    return f"<h1>Get file: {guid}</h1>"
+
+@app.route("/get-file/<string:file_name>")
+def get_file(file_name):
+    print(file_name)
+
+    try:
+        return send_from_directory(app.config["file_storage_path"], filename=file_name, as_attachment = True)
+    except FileNotFoundError:
+        abort(404)
+
+    return(file_name)
+
+@app.route("/remove-file/<string:file_name>")
+def remove_file(file_name):
+    try:
+        os.remove(os.path.join(app.config["file_storage_path"], file_name))
+        return(f"{file_name} removed")
+    except FileNotFoundError:
+        abort(404)
