5e86b11436f114a6851f8dc2d54a3ba6741fd87d
Jiri Hubacek <jirihubacek@Aryas-MacBook-Pro.local>
2020-08-14 00:59:54 +1000

    done

--- app/views.py
+++ app/views.py
@@ -3,26 +3,21 @@ from flask import render_template, request, redirect, send_from_directory, abort
 from werkzeug.utils import secure_filename
 import os
 
-#TBD: put into config,py
-app.secret_key = "super secret key"
-currentDir = os.path.dirname(os.path.realpath(__file__))
-app.config["file_storage_path"] = os.path.join(currentDir,"fileStorage")
-app.config["max_file_size"] = 100 * 1024 * 1024
-
 def allowed_image_filesize(file_size):
-    if int(file_size) <= app.config["max_file_size"]:
+    if int(file_size) <= app.config["MAX_FILE_SIZE"]:
         return True
 
     return False
 
 @app.route("/", methods=["GET", "POST"])
 def home():
+    print(app.config)
     if request.method == "POST":
         print(request.cookies)
         if request.files:
             if not allowed_image_filesize(request.cookies.get("fileSize")):
                 #TODO: do flash message
-                print(f"File exceeded maximum size of {app.config['max_file_size'] / 1024 / 1024}MB")
+                print(f"File exceeded maximum size of {app.config['MAX_FILE_SIZE'] / 1024 / 1024}MB")
                 return redirect(request.url)
 
             file = request.files["file"]
@@ -34,7 +29,7 @@ def home():
 
             file_name = secure_filename(file.filename)
 
-            file.save(os.path.join(app.config["file_storage_path"], file_name))
+            file.save(os.path.join(app.config["FILE_STORAGE_PATH"], file_name))
             flash(f"File uploaded. Link to the file: {request.url_root + 'get_file/' + file_name}" , "info")
             flash(f"To delete the file: {request.url_root + 'remove_file/' + file_name}" , "info")
             return redirect(request.url_root)
@@ -55,7 +50,7 @@ def get_file(file_name):
     print(file_name)
 
     try:
-        return send_from_directory(app.config["file_storage_path"], filename=file_name, as_attachment = True)
+        return send_from_directory(app.config["FILE_STORAGE_PATH"], filename=file_name, as_attachment = True)
     except FileNotFoundError:
         abort(404)
 
@@ -64,7 +59,7 @@ def get_file(file_name):
 @app.route("/remove_file/<string:file_name>")
 def remove_file(file_name):
     try:
-        os.remove(os.path.join(app.config["file_storage_path"], file_name))
+        os.remove(os.path.join(app.config["FILE_STORAGE_PATH"], file_name))
         return(f"{file_name} removed")
     except FileNotFoundError:
         abort(404)
