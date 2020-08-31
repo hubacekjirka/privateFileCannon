8530618eaf403948ede4f444b1e09dd78a5ed266
Jiri Hubacek <jirihubacek@Aryas-MacBook-Pro.local>
2020-09-01 00:36:10 +1000

    done

--- app/views.py
+++ app/views.py
@@ -1,73 +1,105 @@
 from app import app
-from flask import render_template, request, redirect, send_from_directory, abort, flash, jsonify, make_response
+from flask import (
+    render_template,
+    request,
+    redirect,
+    send_from_directory,
+    abort,
+    flash,
+    jsonify,
+    make_response
+)
 from werkzeug.utils import secure_filename
 import os
 
-# Catch 413 error raised when the uploaded file exceeds limit defined in the 
+
+# Catch 413 error raised when the uploaded file exceeds limit defined in the
 # environment variable
 @app.errorhandler(413)
 def file_size_exceeded(e):
-    res = make_response(jsonify({
-                "message": f"The file has exceeded maximum allowed size",
-                }), 413)
+    res = make_response(
+        jsonify(
+            {
+                "message": "The file has exceeded maximum allowed size",
+            }
+        ),
+        413,
+    )
     return res
+
 
 @app.route("/", methods=["GET", "POST"])
 def home():
     print(app.config)
     if request.method == "POST":
 
-        if (not request.form["upload_password"] == app.config["UPLOAD_PASSWORD"]):
-            res = make_response(jsonify({
-                "message": f"Invalid password",
-                }), 403)
+        if not request.form["upload_password"] == app.config["UPLOAD_PASSWORD"]:
+            res = make_response(
+                jsonify(
+                    {
+                        "message": "Invalid password",
+                    }
+                ),
+                403,
+            )
             return res
 
         # Check if there's any file coming through the form
         if not request.files["file"].filename == "":
-            
+
             file = request.files["file"]
             file_name = secure_filename(file.filename)
             file.save(os.path.join(app.config["FILE_STORAGE_PATH"], file_name))
 
-            res = make_response(jsonify({
-                "message": f"{file_name} uploaded",
-                "get_link": f"{request.url_root + 'get_file/' + file_name}",
-                "remove_link": f"{request.url_root + 'remove_file/' + file_name}"
-                }), 200)
+            # Reokace http with https as the app runs behind proxy
+            url_root = request.url_root
+            if app.config["HTTPS_PROXY"]:
+                url_root = url_root.replace("http", "https")
+
+            res = make_response(
+                jsonify(
+                    {
+                        "message": f"{file_name} uploaded",
+                        "get_link": f"{url_root + 'get_file/' + file_name}",
+                        "remove_link": f"{url_root + 'remove_file/'}"
+                        + f"{file_name}",
+                    }
+                ),
+                200,
+            )
             return res
 
         # No file submitted by user - shouldn't happen, safety net on backend
         else:
-            flash(f"No ammunition provided, sorry ...", "error")
+            flash("No ammunition provided, sorry ...", "error")
             return redirect(request.url)
 
     return render_template("upload.html")
+
 
 @app.route("/about")
 def about():
     return render_template("about.html")
 
-#TODO:
-#@app.route("/<uuid:guid>")
-#def download(guid):
-#    return f"<h1>Get file: {guid}</h1>"
 
 @app.route("/get_file/<string:file_name>")
 def get_file(file_name):
     print(file_name)
 
     try:
-        return send_from_directory(app.config["FILE_STORAGE_PATH"], filename=file_name, as_attachment = True)
+        return send_from_directory(
+            app.config["FILE_STORAGE_PATH"], filename=file_name, as_attachment=True
+        )
     except FileNotFoundError:
         abort(404)
 
-    return(file_name)
+    return file_name
+
 
 @app.route("/remove_file/<string:file_name>")
 def remove_file(file_name):
     try:
         os.remove(os.path.join(app.config["FILE_STORAGE_PATH"], file_name))
-        return(f"{file_name} removed")
+        return f"{file_name} removed"
     except FileNotFoundError:
         abort(404)
--- config.py
+++ config.py
@@ -1,19 +1,29 @@
 import os
 
+
 class Config:
     """Base config."""
+
     current_dir = os.path.dirname(os.path.realpath(__file__))
-    SECRET_KEY = os.environ.get('SECRET_KEY')
-    UPLOAD_PASSWORD = os.environ.get('UPLOAD_PASSWORD')
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+    UPLOAD_PASSWORD = os.environ.get("UPLOAD_PASSWORD")
     FILE_STORAGE_PATH = os.path.join(current_dir, "app", "fileStorage")
-    MAX_CONTENT_LENGTH = int(os.environ.get('MAX_FILESIZE_MB')) * 1024 * 1024
+    MAX_CONTENT_LENGTH = int(os.environ.get("MAX_FILESIZE_MB")) * 1024 * 1024
+
+    if os.environ.get("HTTPS_PROXY", "").lower() == "false":
+        HTTPS_PROXY = False
+    else:
+        HTTPS_PROXY = True
+
+
 
 class ProdConfig(Config):
-    FLASK_ENV = 'production'
+    FLASK_ENV = "production"
     DEBUG = False
     TESTING = False
 
+
 class DevConfig(Config):
-    FLASK_ENV = 'development'
+    FLASK_ENV = "development"
     DEBUG = True
     TESTING = True
